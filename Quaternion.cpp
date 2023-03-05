00006f514e26a8f1b94168a56de5f7ac16dba759
TaisaKiarkhi <89452998+TaisaKiarkhi@users.noreply.github.com>
2023-03-05 16:40:05 +0100

    all needed functions added

---
+++ Quaternion.cpp
@@ -0,0 +1,25 @@
+#include "Quaternion.h"
+
+Quaternion::Quaternion()
+{
+}
+
+Quaternion::~Quaternion()
+{
+}
+
+void Quaternion::matrix4x4SetIdentity(Matrix4x4 matIdent4x4)
+{
+}
+
+void Quaternion::translate3D(GLfloat tx, GLfloat ty, GLfloat tz)
+{
+}
+
+void Quaternion::matrix4X4PreMultiply(Matrix4x4 mat1_, Matrix4x4 mat2_)
+{
+}
+
+void Quaternion::rotate3D_Quaternion()
+{
+}
